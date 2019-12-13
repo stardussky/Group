af67e11c4b4e6d8653ed74cf052c1e14fd2252db
stardussky <stardussky@gmail.com>
2019-12-13 18:42:11 +0800

    update

--- v2/homepage-2/js/object.js
+++ v2/homepage-2/js/object.js
@@ -1,11 +1,11 @@
 class CircleButton {
-  constructor(x, y, z, id, color = 0xf6ca83, fontColor = '#fff') {
+  constructor(x, y, z, id, color = 0xf6ca83, fontColor = "#fff") {
     this.x = x;
     this.y = y;
     this.z = z;
     this.id = id;
     this.color = color;
-    this.fontColor = fontColor
+    this.fontColor = fontColor;
     this.opacity = [];
     this.scale = [];
     this.button = new THREE.Group();
@@ -26,19 +26,19 @@ class CircleButton {
     let font = document.getElementById(`${this.id}`);
     font.style.color = this.fontColor;
     this.fontLabel = new THREE.CSS2DObject(font);
-    this.fontLabel.position.set(this.x, this.y + 20, this.z)
-    
+    this.fontLabel.position.set(this.x, this.y + 20, this.z);
+
     this.t1 = new TimelineMax();
     this.t2 = new TimelineMax();
-  }
-  update() {
     this.t1.staggerTo(this.opacity, 3, { opacity: 0 }, 1).repeat(-1);
-    this.t2.staggerFromTo(
-      this.scale,
-      3,
-      { x: 0.1, y: 0.1, z: 0.1 },
-      { x: 15, y: 15, z: 15 },
-      1
-    ).repeat(-1);
+    this.t2
+      .staggerFromTo(
+        this.scale,
+        3,
+        { x: 0.1, y: 0.1, z: 0.1 },
+        { x: 15, y: 15, z: 15 },
+        1
+      )
+      .repeat(-1);
   }
 }
